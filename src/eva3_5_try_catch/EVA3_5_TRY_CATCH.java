e9d62743158194bda31c63dfd9b75162cbf62004
IvanTron <99278095+IvanRoger20@users.noreply.github.com>
2022-05-09 17:42:28 -0600

    Add files via upload

---
+++ src/eva3_5_try_catch/EVA3_5_TRY_CATCH.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva3_5_try_catch;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA3_5_TRY_CATCH {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Introduzca su edad(En numero entero): ");
+        int edad = 0;
+        try{
+                edad  = input.nextInt();}catch(InputMismatchException e){
+                //e.printStackTrace();
+                System.out.println("Oops!! Fallo la captura!!!");
+                }
+            System.out.println("Tu edad: " + edad);
+        }
+    }
